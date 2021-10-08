ae5f7fc92e8751036b81282e7117d42d36a7b7f5
Tung Hoang <tungjty@gmail.com>
2021-10-08 14:39:08 +0700

    master modified Java41_Template_Login.java : add new variable 'loginPage'

--- src/main/java/com/example/hellospringboot/addtemplate/Java41_Template_Login.java
+++ src/main/java/com/example/hellospringboot/addtemplate/Java41_Template_Login.java
@@ -2,6 +2,7 @@ package com.example.hellospringboot.addtemplate;
 
 public class Java41_Template_Login {
     public static void main(String[] args) {
-        System.out.println("This is login page");
+        String loginPage = "login page";
+        System.out.println("This is " + loginPage);
     }
 }
