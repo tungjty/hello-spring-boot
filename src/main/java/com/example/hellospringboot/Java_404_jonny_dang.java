908e0d6a15209cfbd8a8aa45b4a1fed88d135928
Jonny Dang <jonny.dang180284@gmail.com>
2021-10-08 20:54:22 +0700

    Jonny Dang push 03 commit: edit 4.0.4-jonny-dang.java added psvm method

--- src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
+++ src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
@@ -1,4 +1,7 @@
 package com.example.hellospringboot;
 
 public class Java_404_jonny_dang {
+    public static void main(String[] args) {
+        System.out.println("hello 4.0.4-jonny-dang");
+    }
 }
