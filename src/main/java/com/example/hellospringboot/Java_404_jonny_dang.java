2cb9a9dd34a383e404290ea951a091fdea99db75
Jonny Dang <jonny.dang180284@gmail.com>
2021-10-09 11:28:44 +0700

    master in jonny-dang commit: edit 4.0.4-jonny-dang.java added new second sout

--- src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
+++ src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
@@ -3,5 +3,6 @@ package com.example.hellospringboot;
 public class Java_404_jonny_dang {
     public static void main(String[] args) {
         System.out.println("hello 4.0.4-jonny-dang");
+        System.out.println("hello world");
     }
 }
