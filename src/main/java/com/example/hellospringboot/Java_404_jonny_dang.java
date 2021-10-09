09b6818f0831011eb48ad5b87d0af7c11eb0e6de
Jonny Dang <jonny.dang180284@gmail.com>
2021-10-09 15:31:33 +0700

    master branch of 4.0.4-jonny-dang commit: edit 4.0.4-jonny-dang.java added String branch = " jonny-dang-4.0.4";

--- src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
+++ src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
@@ -2,6 +2,7 @@ package com.example.hellospringboot;
 
 public class Java_404_jonny_dang {
     public static void main(String[] args) {
-        System.out.println("this is jonny-dang-4.0.4");
+        String branch = " jonny-dang-4.0.4";
+        System.out.println("this is " + branch);
     }
 }
