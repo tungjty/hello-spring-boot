c34b39c2444f29abd72723b5f15e755aa4eb9612
Jonny Dang <jonny.dang180284@gmail.com>
2021-10-09 15:21:22 +0700

    jonny-dang-4.0.4 commit: edit 4.0.4-jonny-dang.java added sout "hello everyone"

--- src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
+++ src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
@@ -2,6 +2,7 @@ package com.example.hellospringboot;
 
 public class Java_404_jonny_dang {
     public static void main(String[] args) {
+        System.out.println("hello everyone");
         System.out.println("this is jonny-dang-4.0.4");
     }
 }
