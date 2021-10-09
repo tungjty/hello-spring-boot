b4625420192ab4419200ddf9d28eeb72343339c1
Tung Hoang <tungjty@gmail.com>
2021-10-10 06:23:02 +0700

    master modified Java_404_jonny_dang : changes

--- src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
+++ src/main/java/com/example/hellospringboot/Java_404_jonny_dang.java
@@ -4,7 +4,7 @@ public class Java_404_jonny_dang {
     public static void main(String[] args) {
         System.out.println("hello everyone");
         System.out.println("this is jonny-dang-4.0.4");
-        System.out.println("add new in 5.0.5-jonnydang");
+        System.out.println("add new in 4.0.5/documents");
     }
 
     static double sum (int i, int j) {
