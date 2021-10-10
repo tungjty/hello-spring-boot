c955815c0647c1903675ef791a58253e16b4fbcb
Tung Hoang <tungjty@gmail.com>
2021-10-10 13:35:07 +0700

    Alien Ngan post second : edit Java406_alien_ngan.java

--- src/main/java/com/example/hellospringboot/Java406_alien_ngan.java
+++ src/main/java/com/example/hellospringboot/Java406_alien_ngan.java
@@ -1,4 +1,7 @@
 package com.example.hellospringboot;
 
 public class Java406_alien_ngan {
+    public static void main(String[] args) {
+        System.out.println("this is created by 406-draft branch");
+    }
 }
